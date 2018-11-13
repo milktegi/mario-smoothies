1b57b8131fc8a9a3c72f2d83c6596ff91ab5cfce
milktegi <milkteegi@gmail.com>
2018-11-13 16:33:05 +0900

    added Navbar

---
+++ mario-smoothies/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+	<div class="navbar">
+		<nav class="nav-extended red accent-2">
+			<div class="nav-content">
+				<router-link to="">
+					<span class="nav-title">
+						mario smoothies
+					</span>
+				</router-link>
+				<a href="" class="btn-floating btn-large halfway-fab pink">
+					<router-link to="">
+						<!-- 구글 아이콘 + sign-->
+						<i class="material-icons">add</i>	
+					</router-link>
+				</a>
+			</div>
+		</nav>
+	</div>
+
+</template>
+
+<script>
+export default {
+	
+	name: 'Navbar',
+	data() {
+		return {
+
+		}
+	}
+
+}
+</script>
+
+<style>
+
+.navbar nav {
+	padding: 0 30px
+}
+</style>
+
