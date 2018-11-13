d83dbc2cee37a71f8e3e31a2fbee5914b5fc78f5
milktegi <milkteegi@gmail.com>
2018-11-14 03:13:10 +0900

    saving records to firebase

--- mario-smoothies/src/components/AddSmoothie.vue
+++ mario-smoothies/src/components/AddSmoothie.vue
@@ -25,6 +25,10 @@
 </template>
 
 <script>
+
+import db from '@/firebase/init'
+import slugify from 'slugify'
+
 export default {
   name: "AddSmoothie",
   data() {
@@ -32,12 +36,34 @@ export default {
 		title: null,
 		another: null, 
 		ingredients: [],
-		feedback: null
+		feedback: null,
+		slug: null
 	}
   },
   methods: {
 	  AddSmoothie() {
 		  console.log(this.title, this.ingredients)
+		  if(this.title){
+			  this.feedback = null
+			  // create a slug
+			  this.slug = slugify(this.title, {
+				  replacement: '-',
+				  remove: /[$*_+~.()'"!\-:@]/g,
+				  lower: true
+			  })
+			  db.collection('smoothies').add({
+				  title: this.title,
+				  ingredients: this.ingredients,
+				  slug: this.slug
+			  }).then(() => {
+				  this.$router.push({name: 'Index'})
+			  }).catch(err => {
+				  console.log(err)
+			  })
+			  
+		  } else {
+			  this.feedback = "You must enter a smoothie title"
+		  }
 	  },
 	  addIng() {
 		  if(this.another) {
