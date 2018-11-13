3c19f53bba452f927b6576fe76494f46644ab73e
milktegi <milkteegi@gmail.com>
2018-11-14 02:31:26 +0900

    rewrote addSmoothie comp1

--- mario-smoothies/src/components/AddSmoothie.vue
+++ mario-smoothies/src/components/AddSmoothie.vue
@@ -1,63 +1,52 @@
 <template>
 <div class="add-smoothie container">
-	<h4 class="center-align indigo-text">Add New smoothie Recipe</h4>
-	<form @submit.prevent="AddSmoothie">
-		<div class="field title">
-			<label for="title">Smoothie Title</label>
-			<input type="text" name="title" v-model="title">
-		</div>
-		<div class="field add-ingredient">
-			<label for="add-ingredient">Add an ingredient:</label>
-			<input type="text" name="add-ingredient" 
-			@keydown.tab="addIng" v-model="another">
-		</div>
-		<div class="field center-align">
-			<button class="btn blue">Add Smoothie</button>
-		</div>
-	</form>
+<h2 class="center-align indigo-text">Add new smoothie recipe</h2>	
+<form @submit.prevent="AddSmoothie">
+	<div class="field">
+		<label for="title">smoothie title:</label>
+		<input type="text" name="title" v-model="title">
+	</div>
+	<div class="field add-ingredient">
+		<label for="add-ingredient">add an ingredient:</label>
+		<input type="text" name="add-ingredient">
+	</div>
+	<div class="field center align">
+		<button class="btn blue">add smoothie</button>
+	</div>
+</form>
 </div>
-
 </template>
 
 <script>
 export default {
-	name: 'AddSmoothie',
-	data() {
-		return {
-			title: null,
-			another: null,
-			ingredients: []
-		}
-	},
-	methods: {
-		AddSmoothie() {
-			console.log(this.title)
-		},
-		addIng() {
-			if(this.another){
-				this.ingredients.push(this.another)
-				console.log(this.ingredients)
-			}
-		}
-	}
-}
+  name: "AddSmoothie",
+  data() {
+    return {
+		title: null
+	};
+  },
+  methods: {
+	  AddSmoothie() {
+		  console.log(this.title)
+	  }
+  }
+};
 </script>
 
 <style>
 
 .add-smoothie{
-	margin: 60px;
+	margin-top: 60px;
 	padding: 20px;
 	max-width: 500px;
 }
-
 .add-smoothie h2{
 	font-size: 2em;
 	margin: 20px auto;
-
 }
 .add-smoothie .field{
 	margin: 20px auto;
 }
 
 </style>
+
