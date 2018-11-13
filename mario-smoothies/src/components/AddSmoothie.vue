f7208385ebfc14bb68873411e52e0c604f8d9f56
milktegi <milkteegi@gmail.com>
2018-11-14 02:51:37 +0900

    added ingredients

--- mario-smoothies/src/components/AddSmoothie.vue
+++ mario-smoothies/src/components/AddSmoothie.vue
@@ -8,9 +8,12 @@
 	</div>
 	<div class="field add-ingredient">
 		<label for="add-ingredient">add an ingredient:</label>
-		<input type="text" name="add-ingredient">
+		<input type="text" 
+		name="add-ingredient"
+		@keydown.tab.prevent="addIng" v-model="another">
 	</div>
 	<div class="field center align">
+		<p v-if="feedback" class="red">{{ feedback }}</p>
 		<button class="btn blue">add smoothie</button>
 	</div>
 </form>
@@ -22,12 +25,25 @@ export default {
   name: "AddSmoothie",
   data() {
     return {
-		title: null
-	};
+		title: null,
+		another: null, 
+		ingredients: [],
+		feedback: null
+	}
   },
   methods: {
 	  AddSmoothie() {
-		  console.log(this.title)
+		  console.log(this.title, this.ingredients)
+	  },
+	  addIng() {
+		  if(this.another) {
+			  this.ingredients.push(this.another)
+			//   console.log(this.ingredients)
+			this.another = null
+			this.feedback = null
+		  } else {
+			  this.feedback = 'You must enter a value to add an ingredient'
+		  }
 	  }
   }
 };
@@ -36,8 +52,9 @@ export default {
 <style>
 
 .add-smoothie{
-	margin-top: 60px;
-	padding: 20px;
+	margin-top: 30px;
+	/* margin-bottom: 30px; */
+	padding: 40px;
 	max-width: 500px;
 }
 .add-smoothie h2{
@@ -46,6 +63,10 @@ export default {
 }
 .add-smoothie .field{
 	margin: 20px auto;
+}
+
+form {
+	margin-top: 40px;
 }
 
 </style>
