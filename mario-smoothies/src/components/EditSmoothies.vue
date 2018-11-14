094a5646f01cddf48ae13d276c1490af60353b00
milktegi <milkteegi@gmail.com>
2018-11-14 11:29:10 +0900

    firestore queries for edit smoothies

--- mario-smoothies/src/components/EditSmoothies.vue
+++ mario-smoothies/src/components/EditSmoothies.vue
@@ -1,23 +1,40 @@
 <template>
-	<div class="edit-smoothie container">
-		<h2>Edit a smoothie</h2>
-		<h2>{{this.$route.params.smoothie_slug}}</h2>
+	<div v-if="smoothie" class="edit-smoothie container">
+		<h2>Edit {{ smoothie.title }} </h2>
 	</div>
 </template>
 
 <script>
+
+import db from '@/firebase/init'
+
 export default {
 	name: 'EditSmoothie',
 	data() {
 		return {
-
+			smoothie: null
 		}
+	},
+	created() {
+		let ref = db.collection('smoothies')
+		.where('slug', '==', this.$route.params.smoothie_slug)
+		ref.get().then(snapshot => {
+			snapshot.forEach(doc => {
+				console.log(doc.data())
+				this.smoothie = doc.data()
+				this.smoothie.id = doc.id
+			})
+		})
 	}
 }
 </script>
 
 <style>
 
+.edit-smoothie h2{
+	font-size: 2em;
+	margin: 20px auto;
+}
 
 
 </style>
--- mario-smoothies/src/firebase/init.js
+++ mario-smoothies/src/firebase/init.js
@@ -1,4 +1,4 @@
-  import firebase from 'firebase'
+  import firebase from 'firebase/App'
   import firestore from 'firebase/firestore'
   
   // Initialize Firebase
